7b0f4384d61af3b7ce28e7f10b3ba621db890082
Mehdi ABAAKOUK <theli48@gmail.com>
2011-10-25 09:06:30 +0200

    Cleanup browse interface code

--- vnstat.js
+++ vnstat.js
@@ -15,40 +15,17 @@ var request = require('./lib/request');
 
 var urllist = {}
 urllist["/css/style.css"] = request.css;
-
-function map_url(netdev, index, array){
-	if (netdev[0] == ".") return
-
-	console.log("map /"+netdev);
-	urllist["/"+netdev+"/"] = request.home;
-	urllist["/"+netdev+"/hour"] = request.hour;
-	urllist["/"+netdev+"/graph-h"] = request.hourgraph;
-	urllist["/"+netdev+"/day"] = request.day;
-	urllist["/"+netdev+"/graph-d"] = request.daygraph;
-	urllist["/"+netdev+"/week"] = request.week;
-	urllist["/"+netdev+"/graph-w"] = request.weekgraph;
-	urllist["/"+netdev+"/month"] = request.month;
-	urllist["/"+netdev+"/graph-m"] = request.monthgraph;
-	urllist["/"+netdev+"/graph-s"] = request.sumgraph;
-	urllist["/"+netdev+"/graph-t"] = request.topgraph;
-}
-
-vnstat_path = '/var/lib/vnstat'
-fs.readdir(vnstat_path, function(err1, files){
-
-	function redirect(path, response){
-		response.writeHead(302, {
-		'Location': '/'+files[0]+'/'
-		});
-		response.end();
-	}
-	urllist["/"] = redirect;
-
-	if (err1) {
-	} else {
-		files.forEach(map_url);
-	}
-})
+urllist["/%s/"] = request.home;
+urllist["/%s/hour"] = request.hour;
+urllist["/%s/graph-h"] = request.hourgraph;
+urllist["/%s/day"] = request.day;
+urllist["/%s/graph-d"] = request.daygraph;
+urllist["/%s/week"] = request.week;
+urllist["/%s/graph-w"] = request.weekgraph;
+urllist["/%s/month"] = request.month;
+urllist["/%s/graph-m"] = request.monthgraph;
+urllist["/%s/graph-s"] = request.sumgraph;
+urllist["/%s/graph-t"] = request.topgraph;
 
 //**********
 // Main
